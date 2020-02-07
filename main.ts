0ebbe7858dc18579282f2f9d01ea9b8b52236fce
angel calderaro <calderaro94@gmail.com>
2020-02-07 01:36:52 -0600

    fetch

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
+const router = new Router();
 
-app.use(ctx => {
-  ctx.response.body = "Hello World!";
-});
+router
+  .get("/", ctx => {
+    ctx.response.body = "Hello World!";
+  })
+  .get("/pokemon", async ctx => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto/");
+    const data = await response.json();
+    ctx.response.body = data;
+  });
+
+app.use(router.routes());
 
 await app.listen({ port: 8000 });
